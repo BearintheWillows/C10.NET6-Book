21c55f1498ec4b12bf9623874ed9226dd6799f03
BearintheWillows <sjwillows91@outlook.com>
2022-03-20 20:37:12 +0000

    FInished chapter07

---
+++ Chapter07/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+using static System.Console;
+
+Write( "Please enter your age:" );
+string? input = ReadLine();
+
+/**
+ * ^ - Start of input
+ * \d - digit
+ * + - More than one
+ * $ - End of input
+ */
+
+Regex ageChecker = new(@"^\d+$");
+
+if ( ageChecker.IsMatch( input ) )
+{
+    WriteLine( "Thank you!" );
+}
+else
+{
+    WriteLine( $"This is not a valid age: {input}" );
+}
+
+
+String? films = "\"Monsters, Inc.\",\"I, Tonya\",\"Lock, Stock and Two Smoking Barrels\"";
+
+WriteLine( $"Films to split: {films}" );
+
+string[]filmsDumb = films.Split(',');
+
+WriteLine( "Splitting with string.split" );
+
+foreach ( var film in filmsDumb )
+{
+    WriteLine( film );
+}
