51f0ce4995d07e35283c5a859b5a2394961dc282
BearintheWillows <sjwillows91@outlook.com>
2022-05-21 12:06:57 +0100

    Added auto gen models

--- Chapter10/Northwind.cs
+++ Chapter10/Northwind.cs
@@ -1,9 +1,15 @@
 using Microsoft.EntityFrameworkCore;
+using WorkingWithEFCore.Models;
 using static System.Console;
 
 namespace WorkingWithEFCore;
 
 public class Northwind {
+	
+	//Props map to the tables in the database
+	public DbSet<Category>? Categories { get; set; }
+	public DbSet<Product>? Products { get; set; }
+
 	protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
 		if ( ProjectConstants.DbProvider == "SQLite" ) {
 			var path = Path.Combine( Environment.CurrentDirectory, "Northwind.db" );
@@ -18,5 +24,23 @@ public class Northwind {
 			                 "MultipleActiveResultSets=true;";
 			optionsBuilder.UseSqlServer( connection );
 		}
+	}
+
+	protected void OnModelCreating(ModelBuilder modelBuilder) {
+	
+		// Fluent API instead of attributes to limit length of cat name
+
+		modelBuilder.Entity<Category>()
+		            .Property(category => category.CategoryName )
+		            .IsRequired() // Not null
+		            .HasMaxLength( 15 );
+		
+		//Used to fix SQLites lack of decimal support
+		if ( ProjectConstants.DbProvider == "SQLite" ) {
+			modelBuilder.Entity<Product>()
+			            .Property( product => product.Cost )
+			            .HasConversion<double>();
+		}
+
 	}
 }
