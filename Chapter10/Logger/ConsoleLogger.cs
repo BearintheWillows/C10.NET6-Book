52c9894fa1c6dadabe5d23f376ae97e2b9b492ae
BearintheWillows <sjwillows91@outlook.com>
2022-05-29 11:54:25 +0100

    Added Logger to Program.cs

--- Chapter10/Logger/ConsoleLogger.cs
+++ Chapter10/Logger/ConsoleLogger.cs
@@ -2,49 +2,63 @@ namespace WorkingWithEFCore.Logger;
 
 using Microsoft.Extensions.Logging;
 
-public class ConsoleLoggerProvider : ILoggerProvider {
-	public ILogger CreateLogger(string categoryName) {
-		/**
+public class ConsoleLoggerProvider : ILoggerProvider
+{
+    public ILogger CreateLogger(string categoryName)
+    {
+        /**
 		 * We could have different logger implementations for
 		 * different categoryName values but for simplicity
 		 * we will just return the same logger for all
 		 */
 
-		return new ConsoleLogger();
-	}
+        return new ConsoleLogger();
+    }
 
-	//If the logger provider needs to do any cleanup, it should do it here
+    //If the logger provider needs to do any cleanup, it should do it here
 
-	public void Dispose() { }
+    public void Dispose() { }
 
 }
-public class ConsoleLogger : ILogger {
-	//If the logger needs to do any cleanup, it should do it here
-	
-public IDisposable BeginScope<TState>(TState state) {
-		return null;
-	}	
+public class ConsoleLogger : ILogger
+{
+    //If the logger needs to do any cleanup, it should do it here
 
-public bool IsEnabled(LogLevel logLevel) {
-	switch ( logLevel ) {
-	case LogLevel.Trace:
-		case LogLevel.Information:
-		case LogLevel.None:
-			return false;
-	case LogLevel.Debug:
-		case LogLevel.Warning:
-		case LogLevel.Error:
-		case LogLevel.Critical:
-		default:
-			return true;
-	}
-	}
+    public IDisposable BeginScope<TState>(TState state)
+    {
+        return null;
+    }
 
-public void Log<TState>(LogLevel loglevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception, string> formatter) {
-	Console.WriteLine($"{loglevel} - {eventId} - {formatter(state, exception)}");
-	}
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        switch (logLevel)
+        {
+            case LogLevel.Trace:
+            case LogLevel.Information:
+            case LogLevel.None:
+                return false;
+            case LogLevel.Debug:
+            case LogLevel.Warning:
+            case LogLevel.Error:
+            case LogLevel.Critical:
+            default:
+                return true;
+        };
+    }
 
-if (state != null) {
-	Console.Write( $"" );
-}
+    public void Log<TState>(LogLevel loglevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception, string> formatter)
+    {
+        Console.WriteLine($"{loglevel} - {eventId} - {formatter(state, exception)}");
+
+
+        if (state != null)
+        {
+            Console.Write($", State: {state}");
+        }
+        if (exception != null)
+        {
+            Console.Write($", Exception: {exception.Message}");
+        }
+        Console.WriteLine();
+    }
 }
