335c7cf4776373f64591b1bff7bbc92898517951
BearintheWillows <sjwillows91@outlook.com>
2022-06-04 11:47:25 +0100

    Added Join. Starting Groupjoin

--- Chapter11/LinqWithEFCore/Program.cs
+++ Chapter11/LinqWithEFCore/Program.cs
@@ -2,7 +2,9 @@
 using Microsoft.EntityFrameworkCore; //DbSet<T>
 using static System.Console;
 
-FilterAndSort();
+//FilterAndSort();
+//JoinCategoriesAndProducts();
+GroupJoinCategoriesAndProducts();
 
 static void FilterAndSort()
 {
@@ -31,4 +33,61 @@ static void FilterAndSort()
         WriteLine( $" {item.ProductId}: {item.ProductName} costs {item.UnitPrice: $#,##0.00}" );
     }
     WriteLine();
+}
+
+
+//Joining and Group Join
+
+static void JoinCategoriesAndProducts()
+{
+    Northwind db = new();
+
+    //Join every products to its category to return 77 matches
+
+    var queryJoin = db.Categories.Join(
+        inner: db.Products,
+        outerKeySelector: c => c.CategoryId,
+        innerKeySelector: p => p.CategoryId,
+        resultSelector: (c, p) => new
+        {
+            CategoryName = c.CategoryName,
+            ProductName = p.ProductName,
+            ProductId = p.ProductId
+        })
+        .OrderBy(cp => cp.CategoryName);
+
+    foreach ( var item in queryJoin )
+    {
+        WriteLine( "{0}: {1} is in {2}.",
+            item.ProductId, item.ProductName, item.CategoryName );
+    }
+}
+
+// Group Join
+
+static void GroupJoinCategoriesAndProducts()
+{
+    Northwind db =new();
+
+    //Group all products by their category
+
+    var queryGroup = db.Categories.AsEnumerable().GroupJoin(
+        inner: db.Products,
+        outerKeySelector: c => c.CategoryId,
+        innerKeySelector: p => p.CategoryId,
+        resultSelector: (c, matchingProducts) => new
+        {
+            CategoryName = c.CategoryName,
+            Products = matchingProducts.OrderBy(p => p.ProductName)
+        });
+
+    foreach ( var item in queryGroup )
+    {
+        WriteLine( "{0} has {1} products.",
+            item.CategoryName, item.Products.Count() );
+        foreach ( var product in item.Products )
+        {
+            WriteLine( $" {product.ProductName}" );
+        }
+    }
 }
