c5596a4824b1ac5fc660fa0afc07f89f9a12a452
BearintheWillows <sjwillows91@outlook.com>
2022-06-08 16:56:13 +0100

    Attached database using code-behind file and Injected database straight into cshtml

---
+++ PracticalApps/Northwind.Web/Pages/Suppliers.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Packt.Shared;
+
+namespace Northwind.Web.Pages
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class SuppliersModel : PageModel
+    {
+        [BindProperty]
+        public  Supplier?              Supplier  { get; set; }
+        public  IEnumerable<Supplier>? Suppliers { get; set; }
+        private NorthwindContext       db;
+        
+        public SuppliersModel(NorthwindContext injectedContext)
+        {
+            db = injectedContext;
+        }
+
+        public void OnGet()
+        {
+            ViewData["Title"] = "Northwind B2B Suppliers";
+
+            Suppliers = db.Suppliers
+                          .OrderBy( c => c.Country )
+                          .ThenBy( c => c.CompanyName );
+        }
+
+        public IActionResult OnPost() {
+            if ( ( Supplier != null ) && ModelState.IsValid ) {
+                db.Suppliers.Add( Supplier );
+                db.SaveChanges();
+                return RedirectToPage( "/suppliers" );
+            } else {
+                return Page(); //return to original page
+            }
+        }
+    }
+}
