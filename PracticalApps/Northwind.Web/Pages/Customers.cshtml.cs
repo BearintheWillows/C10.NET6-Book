1ec1bca97bc90d0499ba5242b3a1007ce0f8b3da
BearintheWillows <sjwillows91@outlook.com>
2022-06-09 21:14:11 +0100

    Added GetCustomerById - Ability to retrieve a specific customer from database

--- PracticalApps/Northwind.Web/Pages/Customers.cshtml.cs
+++ PracticalApps/Northwind.Web/Pages/Customers.cshtml.cs
@@ -22,12 +22,21 @@ public class CustomersModel : PageModel {
 		ViewData[ "Title" ] = "Potential Customers at B2B Northwind";
 
 		Customers = await GetData();
+	Customer = await GetCustomerById( "BOLID" );
 
-		return Page();
+	Console.WriteLine(Customer.CompanyName);
+	return Page();
 	}
 
 	public Task<IOrderedQueryable<Customer>>  GetData() {
-	Task<IOrderedQueryable<Customer>> result = Task.FromResult( db.Customers.OrderBy( c => c.Country ).ThenBy( c => c.CompanyName ) );
+		Task<IOrderedQueryable<Customer>> result = Task.FromResult( db.Customers.OrderBy( c => c.Country ).ThenBy( c => c.CompanyName ) );
 		return result ;
+	}
+
+	public Task<Customer> GetCustomerById(string customerId) {
+	
+		Task<Customer> customer = Task.FromResult( db.Customers.Single(c => c.CustomerId == customerId) );
+		
+		return customer;
 	}
 }
