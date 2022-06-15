fa5f8e6003b80392204b3410515952f3b1ad0ada
BearintheWillows <sjwillows91@outlook.com>
2022-06-15 10:41:15 +0100

    Update

---
+++ PracticalApps/Northwind.WebApi/Repositories/ICustomerRepository.cs
@@ -0,0 +1,13 @@
+
+namespace Northwind.WebApi.Repositories;
+
+using Packt.Shared;
+
+public interface ICustomerRepository
+{
+	Task<Customer?>             CreateAsync(Customer c);
+	Task<IEnumerable<Customer>> RetrieveAllAsync();
+	Task<Customer?>             RetrieveAsync(string id);
+	Task<Customer?>             UpdateAsync(string   id, Customer c);
+	Task<bool?>                 DeleteAsync(string   id);
+}
